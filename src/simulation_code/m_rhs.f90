1e0c4c5814dca6ad5725fe904f98036b9aa0c4f6
Mauro Rodriguez <mrdz@umich.edu>
2020-09-01 16:17:09 -0700

    git pushing test

--- src/simulation_code/m_rhs.f90
+++ src/simulation_code/m_rhs.f90
@@ -4374,6 +4374,8 @@ MODULE m_rhs
                                     q_cons_vf(i+adv_idx%beg-1)%sf(j,k,l) = q_cons_vf(i+cont_idx%beg-1)%sf(j,k,l) &
                                         / rho_K_s(i)
                             END DO
+
+
                         END IF
 
                         ! ==================================================================
