7e5b764312376daf7cb475038a740ddf1c311e65
mrodri97 <mauro_rodriguez@brown.edu>
2021-10-14 15:53:50 -0400

    rectified LAPACK BLAS linker configuration name issue

--- installers/install_lapack.sh
+++ installers/install_lapack.sh
@@ -5,9 +5,11 @@ homedir=$(pwd)
 cd lib
 
 library=lapack-3.4.2
+echo 'opening tarball'
 tar -zxf $library'.tgz'
+echo 'finished opening tarball'
 cd $library
-    # cp ../make_lapack.inc make.inc
+    #cp make.inc.example make.inc
     cat ../make_lapack.inc | sed -e s+XXMAKEDIR+$homedir+g > make.inc
     make blaslib 
     make lapacklib 
