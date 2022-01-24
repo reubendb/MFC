a0eb9493479ef4699570cd55957f88ee0ec67096
Spencer Bryngelson <sbryngelson@gmail.com>
2022-01-24 16:12:41 -0500

    fix that last test

--- tests/vacuum_generation/input.py
+++ tests/vacuum_generation/input.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python3
 
-
-# MFC v3.0 - Cases - Kapila model - Vacuum - 1D
-# Description: Vacuum.
-# Author: Kevin Schmidmayer
-# Date: 02/26/18
-
-
 # Dependencies and Logistics ===================================================
 
 # Command to navigate between directories
@@ -58,19 +51,19 @@ case_dict =                                                                    \
                     'p'                            : 0,                        \
                     'dt'                           : 5.E-08,                   \
                     't_step_start'                 : 0,                        \
-                    't_step_stop'                  : 1000,                    \
-                    't_step_save'                  : 1000,                     \
-		    # ==========================================================
+                    't_step_stop'                  : 3000,                     \
+                    't_step_save'                  : 3000,                     \
+                    # ==========================================================
                                                                                \
                     # Simulation Algorithm Parameters ==========================
                     'num_patches'                  : 2,                        \
                     'model_eqns'                   : 2,                        \
                     'alt_soundspeed'               : 'T',                      \
                     'num_fluids'                   : 2,                        \
-		    'adv_alphan'                   : 'T',                      \
-		    'mpp_lim'                      : 'T',                      \
-		    'mixture_err'                  : 'T',                      \
-		    'time_stepper'                 : 3,                        \
+                    'adv_alphan'                   : 'T',                      \
+                    'mpp_lim'                      : 'T',                      \
+                    'mixture_err'                  : 'T',                      \
+                    'time_stepper'                 : 3,                        \
                     'weno_vars'                    : 2,                        \
                     'weno_order'                   : 3,                        \
                     'weno_eps'                     : 1.E-16,                   \
@@ -78,7 +71,7 @@ case_dict =                                                                    \
                     'mapped_weno'                  : 'T',                      \
                     'null_weights'                 : 'F',                      \
                     'mp_weno'                      : 'F',                      \
-		    'riemann_solver'               : 2,                        \
+                    'riemann_solver'               : 2,                        \
                     'wave_speeds'                  : 1,                        \
                     'avg_state'                    : 2,                        \
                     'commute_err'                  : 'F',                      \
@@ -94,14 +87,14 @@ case_dict =                                                                    \
                     'parallel_io'                  :'F',                       \
                     # ==========================================================
 
-		    # Patch 1: Left state ======================================
+                    # Patch 1: Left state ======================================
                     'patch_icpp(1)%geometry'       : 1,                        \
                     'patch_icpp(1)%x_centroid'     : 0.25E+00,                 \
                     'patch_icpp(1)%length_x'       : 0.5E+00,                  \
-                    'patch_icpp(1)%vel(1)'         : -100.E+00,                \
-                    'patch_icpp(1)%pres'           : 1.E+05,                   \
-                    'patch_icpp(1)%alpha_rho(1)'   : 1000.E+00*0.99E+00,       \
-                    'patch_icpp(1)%alpha_rho(2)'   : 10.E+00*0.01E+00,         \
+                    'patch_icpp(1)%vel(1)'         : -10.E+00,                 \
+                    'patch_icpp(1)%pres'           : 1.E+02,                   \
+                    'patch_icpp(1)%alpha_rho(1)'   : 10.E+00*0.99E+00,         \
+                    'patch_icpp(1)%alpha_rho(2)'   : 1.E+00*0.01E+00,          \
                     'patch_icpp(1)%alpha(1)'       : 0.99E+00,                 \
                     'patch_icpp(1)%alpha(2)'       : 0.01E+00,                 \
                     # ==========================================================
@@ -111,20 +104,20 @@ case_dict =                                                                    \
                     'patch_icpp(2)%x_centroid'     : 0.75E+00,                 \
                     'patch_icpp(2)%length_x'       : 0.5E+00,                  \
                     'patch_icpp(2)%alter_patch(1)' : 'T',                      \
-                    'patch_icpp(2)%vel(1)'         : 100.E+00,                 \
-                    'patch_icpp(2)%pres'           : 1.E+05,                   \
-                    'patch_icpp(2)%alpha_rho(1)'   : 1000.E+00*0.99E+00,       \
-                    'patch_icpp(2)%alpha_rho(2)'   : 10.E+00*0.01E+00,         \
+                    'patch_icpp(2)%vel(1)'         : 10.E+00,                  \
+                    'patch_icpp(2)%pres'           : 1.E+02,                   \
+                    'patch_icpp(2)%alpha_rho(1)'   : 10.E+00*0.99E+00,         \
+                    'patch_icpp(2)%alpha_rho(2)'   : 1.E+00*0.01E+00,          \
                     'patch_icpp(2)%alpha(1)'       : 0.99E+00,                 \
                     'patch_icpp(2)%alpha(2)'       : 0.01E+00,                 \
                     # ==========================================================
 
-		    # Fluids Physical Parameters ===============================
+                    # Fluids Physical Parameters ===============================
                     'fluid_pp(1)%gamma'            : 1.E+00/(4.4E+00-1.E+00),  \
                     'fluid_pp(1)%pi_inf'           : 4.4E+00*6.E+08/(4.4E+00-1.E+00), \
                     'fluid_pp(2)%gamma'            : 1.E+00/(1.4E+00-1.E+00),  \
                     'fluid_pp(2)%pi_inf'           : 0.E+00,                   \
-	            # ==========================================================
+                    # ==========================================================
 
     }
 
